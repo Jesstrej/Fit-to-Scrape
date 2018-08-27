b0587e27be747b152722623b37bfeafa79adbff2
Jesstrej <jessica16tor@yahoo.com>
2018-08-26 22:12:46 -0700

    update

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 const app = express();
+
+// IMPORT ROUTES HERE
+const homeRoute = require('./routes/home.routes');
+
 
 const PORT = process.env.PORT || 3000;
 
+// ROUTES
+app.use(homeRoute);
 
-app.listen( PORT, () => `localhost ${PORT}`);
+
+app.listen( PORT, () => console.log(`localhost ${PORT}`));
